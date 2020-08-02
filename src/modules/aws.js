42a35405a9ed16df6fe6056897cb22daec21e4d5
Edward Romero <legolamel@gmail.com>
2020-08-01 20:37:15 -0400

    log

--- src/modules/aws.js
+++ src/modules/aws.js
@@ -3,7 +3,7 @@ const aws = require("aws-sdk");
 const serviceConfig = require("./config");
 
 const config = serviceConfig.get("aws.config");
-
+console.log(config);
 const awsConfig = new aws.Config(config);
 // const dynamoClient = aws.DynamoDb
 const sqsClient = new aws.SQS(awsConfig);
