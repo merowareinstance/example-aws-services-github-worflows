c3872bff1348ed78707934af66d0bf8502516f37
Edward Romero <legolamel@gmail.com>
2020-08-01 21:09:28 -0400

    add logging for debug

--- src/services/__tests__/forest.test.js
+++ src/services/__tests__/forest.test.js
@@ -13,17 +13,25 @@ describe("Forest Service Integration Test", () => {
         long: "23.823650",
       };
       objectKey = `tree${uuid.v4()}Location.json`;
-      await aws.s3Client
-        .putObject({
-          Body: JSON.stringify(sampleObj),
-          Bucket: config.get("aws.s3.bucketName"),
-          Key: objectKey,
-        })
-        .promise();
+      try {
+        await aws.s3Client
+          .putObject({
+            Body: JSON.stringify(sampleObj),
+            Bucket: config.get("aws.s3.bucketName"),
+            Key: objectKey,
+          })
+          .promise();
+      } catch (e) {
+        console.log('beforeEach', e);
+      }
     });
     test("Should succeed in getting proper tree location from bucket file", async () => {
-      const data = await getTreeLocationFromS3(objectKey);
+      try {
+        const data = await getTreeLocationFromS3(objectKey);
       expect(data).toMatchObject(sampleObj);
+      } catch(e) {
+          console.log('TEST', e);
+      }
     });
   });
 });
